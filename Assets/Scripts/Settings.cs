6302f6ab16f9e6804e1ac07091351104811fbca3
DMarci <0dmarci00@gmail.com>
2024-04-24 22:11:54 +0200

    Sound Toggle

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    public void SoundToggle(bool mute)
+    {
+        if (!mute)
+        {
+            AudioListener.volume = 0;
+
+        }
+        else { AudioListener.volume = 1; }
+    }
+}
